3e7922be186040d7d4416e959f917354ea9fdfb5
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-03-28 19:57:59 -0400

    Navbar bug fixed and graceful Error Handling added

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,11 +1,16 @@
 
-const ErrorPage = () => {
+import { Link } from 'react-router-dom';
+
+const ErrorPage: React.FC = () => {
     return (
-        <div className="page-container">
-            <h1>Error</h1>
-            <p>Oops! Something went wrong.</p>
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Oops! The page you are looking for has disappeared or never existed.</p>
+            <p>
+                <Link to="/">Go to the Home Page</Link>
+            </p>
         </div>
     );
-}
+};
 
 export default ErrorPage;
