39b2766353179b9c4ffc0b02295219c0d472167b
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-03-27 21:28:48 -0400

    ui set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-
-
 import HomePage from './pages/Homepage';
 import Profile from './pages/Profile';
 import Architects from './pages/Architects';
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,20 +1,12 @@
 
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Assuming you will create a separate CSS file for styling
+import './Footer.css'; // Assuming you will create a separate CSS file for styling
 
-const Navbar = () => {
+const Footer = () => {
     return (
-        <nav className="navbar">
-            <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/architects">Architects</Link></li>
-                <li><Link to="/rules">Rules</Link></li>
-                <li><Link to="/privacy">Privacy</Link></li>
-                <li><Link to="/weaver-info">About Weaver</Link></li>
-            </ul>
-        </nav>
+        <footer className="footer">
+            <p>© 2025 Weaver | Cyrl Casalme, Stella Nabajja, Corey Parsons | All rights reserved.</p>
+        </footer>
     );
 }
 
-export default Navbar;
+export default Footer;
