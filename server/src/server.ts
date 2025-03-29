0452b384207336f0944f5d1c2fdf05fc044155af
Star <snabajja@gmail.com>
2025-03-29 16:22:50 -0400

    Refactor server.ts to use Apollo Server for GraphQL API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,45 @@
-import express, { Application } from 'express';
+import express from 'express';
 import path from 'node:path';
-import dotenv from 'dotenv';
-import db from './config/connection.js';
-//import authRoutes from './routes/auth.js'; // ✅ Ensure this is importing the Router
+import type { Request, Response } from 'express';
+import db from './config/connection.js'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './utils/auth.js';
 
-dotenv.config();
-
-await db();
-
-const app: Application = express(); // ✅ Ensure this is an Express app
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// ✅ Register Routes Properly
-// app.use('/api/auth', authRoutes); // ✅ This must be a Router, not a function!
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}!`);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
+
+startApolloServer();
