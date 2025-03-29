5cf61e99e829b19d6dcfec88d18ffcc0968e0f67
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-03-29 14:19:09 -0400

    wireframe incoporated to the background

--- client/src/pages/Architects.tsx
+++ client/src/pages/Architects.tsx
@@ -1,7 +1,9 @@
+import './Architects.css'; // Import the CSS file for styling
+import Wireframe from "../assets/WireFrameBackground.png"; // Import the background image
 
 const Architects = () => {
     return (
-        <div className="page-container">
+        <div className="page-container" style={{ backgroundImage: `url(${Wireframe})` }}>
             <h1>Architects</h1>
             <p>This page is dedicated to the architects of our community.</p>
         </div>
