288bfca92b7a5c80ea5c0d6082a6172980c48af9
Star <snabajja@gmail.com>
2025-03-31 16:03:57 -0400

    Add index.ts to model folder and import models

---
+++ server/src/models/index.ts
@@ -0,0 +1,8 @@
+import User from './user';
+import Profile from './profile';
+import Story from './story';
+import Comment from './comment';
+import Prompt from './prompt';
+import Vote from './vote';
+
+export { User, Profile, Story, Comment, Prompt, Vote };
