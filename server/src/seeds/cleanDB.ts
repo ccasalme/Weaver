0323a529ca4335435520db4ea8000a7f81d5366c
Star <snabajja@gmail.com>
2025-03-31 17:47:07 -0400

    Add file formatting

--- server/src/seeds/cleanDB.ts
+++ server/src/seeds/cleanDB.ts
@@ -6,16 +6,22 @@ const cleanDB = async (): Promise<void> => {
     try {
         await User.deleteMany({});
         console.log('User collection cleaned!');
+
         await Profile.deleteMany({});
         console.log('Profile collection cleaned!');
+
         await Story.deleteMany({});
         console.log('Story collection cleaned!');
+
         await Comment.deleteMany({});
         console.log('Comment collection cleaned!');
+
         await Prompt.deleteMany({});
         console.log('Prompt collection cleaned!');
+
         await Vote.deleteMany({});
         console.log('Vote collection cleaned!');
+        
     } catch (err) {
        console.error('Error cleaning database:', err);
        process.exit(1);
