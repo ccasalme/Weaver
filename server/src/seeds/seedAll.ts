2984ae5ec8d0334daa4a1915203cf5c76659e5cd
Star <snabajja@gmail.com>
2025-03-31 16:34:54 -0400

    Add seedAll.ts file

---
+++ server/src/seeds/seedAll.ts
@@ -0,0 +1,57 @@
+import db from '../config/connection.js';
+import User from '../models/User.js';
+import Profile from '../models/Profile.js';
+import Story from '../models/Story.js';
+import Comment from '../models/Comment.js';
+import Prompt from '../models/Prompt.js';
+import Vote from '../models/Vote.js';
+import cleanDB from './cleanDB.js';
+
+import userData from './userData.json' assert { type: 'json' };
+import profileData from './profileData.json' assert { type: 'json' };
+import storyData from './storyData.json' assert { type: 'json' };
+import commentData from './commentData.json' assert { type: 'json' };
+import promptData from './promptData.json' assert { type: 'json' };
+import voteData from './voteData.json' assert { type: 'json' };
+
+const seedDatabase = async (): Promise<void> => {
+  try {
+    await db();
+    await cleanDB();
+
+    const users = await User.create(userData);
+    const profiles = await Profile.insertMany(
+      profileData.map((profile, i) => ({ ...profile, user: users[i]._id }))
+    );
+
+    const stories = await Story.insertMany(
+      storyData.map((story, i) => ({ ...story, author: users[i % users.length]._id }))
+    );
+
+    await Comment.insertMany(
+      commentData.map((comment, i) => ({
+        ...comment,
+        author: users[i % users.length]._id,
+        story: stories[i % stories.length]._id
+      }))
+    );
+
+    await Prompt.insertMany(promptData);
+
+    await Vote.insertMany(
+      voteData.map((vote, i) => ({
+        ...vote,
+        user: users[i % users.length]._id,
+        story: stories[i % stories.length]._id
+      }))
+    );
+
+    console.log('Seeding completed successfully!');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
+};
+
+seedDatabase();
