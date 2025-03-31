d4ae20d121a9a70dffbd02521e13f91c77bc850d
Star <snabajja@gmail.com>
2025-03-31 11:58:02 -0400

    Delete comments in resolvers.ts

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -38,13 +38,14 @@ const resolvers = {
     
         // Creates a new user, signs a token for them, and returns both.
         addUser: async (_: any, { username, email, password }: { username: string; email: string; password: string }) => {
-            // First we create the user in the database
+
           const user = await User.create({ username, email, password });
           if (!user) {
             throw new Error("Something is wrong!");
           }
+          
           console.log('addedUser')
-          // Sign a token for the new user.
+
           const token = signToken(user.username, user.email, user._id);
           return { token, user };
         },
