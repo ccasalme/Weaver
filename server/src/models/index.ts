10b5a8ae3f9c4eaef58ce57a343309d71b5465c9
Star <snabajja@gmail.com>
2025-03-31 16:51:43 -0400

    Add js extension to import statements

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,8 +1,8 @@
-import User from './user';
-import Profile from './profile';
-import Story from './story';
-import Comment from './comment';
-import Prompt from './prompt';
-import Vote from './vote';
+import User from './user.js';
+import Profile from './profile.js';
+import Story from './story.js';
+import Comment from './comment.js';
+import Prompt from './prompt.js';
+import Vote from './vote.js';
 
 export { User, Profile, Story, Comment, Prompt, Vote };
