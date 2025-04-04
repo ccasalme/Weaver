ac58a21d1d0ff06bd4bc2f0287695ba4d2ae658e
ccasalme <78170262+ccasalme@users.noreply.github.com>
2025-04-03 22:19:15 -0400

    importing css in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "./pages/Wireframe.css";
 import App from "./App";
 import { ApolloProvider } from "@apollo/client"; // ✅ This line!
 import client from "./utils/apolloClient";        // ✅ Your exported Apollo client
